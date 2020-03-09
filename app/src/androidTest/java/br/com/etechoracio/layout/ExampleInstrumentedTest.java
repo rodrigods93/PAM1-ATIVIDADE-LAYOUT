9325f43f61c6d4211ed5c974223ee794a1005c30
rodrigods93 <rodrigodds96@gmail.com>
2020-03-09 20:40:27 -0300

    Layout Concluido

--- app/src/androidTest/java/br/com/etechoracio/layout/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/br/com/etechoracio/layout/ExampleInstrumentedTest.java
@@ -1,6 +1,7 @@
 package br.com.etechoracio.layout;
 
 import android.content.Context;
+
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
