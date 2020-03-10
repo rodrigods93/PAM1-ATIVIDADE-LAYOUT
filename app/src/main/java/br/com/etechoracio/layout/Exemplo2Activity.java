c56b3fe4f665da34cef68001808ba74392026244
rodrigods93 <rodrigodds96@gmail.com>
2020-03-09 22:26:11 -0300

    Inclusao de novo layout

---
+++ app/src/main/java/br/com/etechoracio/layout/Exemplo2Activity.java
@@ -0,0 +1,14 @@
+package br.com.etechoracio.layout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Exemplo2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_exemplo2);
+    }
+}
